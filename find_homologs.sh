5f1f6382b5d38fb8bed473b0132207ed27638b8f
Rachel Pinto <rachelpinto@Rachels-MacBook-Pro.local>
2026-09-21 16:54:24 -0400

    Remove empty lines

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-tblastn -query "$1" -subject "$2" -outfmt "6 qseqid sseqid pident length qlen" | \
-awk '$3 > 30 && $4 > ($5 * 0.9)' > "$3"
-
+tblastn -query "$1" -subject "$2" -outfmt "6 qseqid sseqid pident length qlen" | awk '$3 > 30 && $4 > ($5 * 0.9)' > "$3"
 wc -l < "$3"
